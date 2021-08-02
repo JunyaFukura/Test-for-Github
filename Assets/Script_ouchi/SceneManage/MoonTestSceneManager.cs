058ac84efd97f9a3923fb1a6fb57d27fe796c499
JunyaFukura <j-fukura@mail.benesse.co.jp>
2021-08-02 10:04:08 +0900

    First Commit
    First Commit by Junya

---
+++ Assets/Script_ouchi/SceneManage/MoonTestSceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoonTestSceneManager : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MoonIntro"); //未定
+    }
+}
